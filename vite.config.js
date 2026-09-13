e73682c699cb7f52972edab70d19d401ed046a17
kfeeney1 <114672059+kfeeney1@users.noreply.github.com>
2026-09-13 19:32:13 +0000

    Make Vite output desktop-file compatible

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 const packageJson = JSON.parse(readFileSync(fileURLToPath(new URL('./package.json', import.meta.url)), 'utf8'))
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   define: {
     'globalThis.__TORNADO_APP_VERSION__': JSON.stringify(packageJson.version),

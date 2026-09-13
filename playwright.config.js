27b769c78e079d8b790b6c8dfa153a5299f1a3c8
kfeeney1 <114672059+kfeeney1@users.noreply.github.com>
2026-09-13 16:01:53 +0000

    Stage 4: scope Playwright to browser specs

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: '**/*.spec.js',
   timeout: 30000,
   use: { baseURL: 'http://127.0.0.1:4173' },
   webServer: {
